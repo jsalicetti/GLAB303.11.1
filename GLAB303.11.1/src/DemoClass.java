faa4e8afb30548802eaac7bf01203012ec4da13d
jsalicetti <jessicasalicetti@yahoo.com>
2024-08-05 09:18:32 -0400

    completed

---
+++ GLAB303.11.1/src/DemoClass.java
@@ -0,0 +1,9 @@
+Public class DemoClass {
+
+    // create a generics method
+    public <T> void genericsMethod(T data) {
+        System.out.println("Generics Method:");
+        System.out.println("Data Passed: " + data);
+    }
+}
+
